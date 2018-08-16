13227f434b12f4188212c7f294a1a68c794a54f9
Leonov Daniil <fireman2002lds@gmail.com>
2018-08-16 15:18:38 +0300

    Add a navigation for open drawer

--- app/src/main/kotlin/com/games/yshmgrt/gok/activities/mainactivity/MainActivity.kt
+++ app/src/main/kotlin/com/games/yshmgrt/gok/activities/mainactivity/MainActivity.kt
@@ -5,21 +5,18 @@ import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.view.MenuItem
 import com.games.yshmgrt.gok.R
 import com.games.yshmgrt.gok.activities.mainactivity.views.MainView
 import com.games.yshmgrt.gok.activities.mainactivity.views.fragments.LessonsListFragment
 import com.games.yshmgrt.gok.activities.mainactivity.views.fragments.LevelListFragment
 import com.games.yshmgrt.gok.loader.DataLoader
-import org.jetbrains.anko.appcompat.v7.navigationIconResource
 import org.jetbrains.anko.find
-import org.jetbrains.anko.findOptional
 import org.jetbrains.anko.setContentView
 
 class MainActivity : AppCompatActivity(){
 
-    private lateinit var drawer : DrawerLayout
+    lateinit var drawer : DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +24,6 @@ class MainActivity : AppCompatActivity(){
         MainView().setContentView(this)
 
         drawer = find(R.id.drawer_layout_main_activity)
-
-        val toolbar = findOptional<Toolbar>(R.id.toolbar_main_activity)
-        toolbar?.apply {
-            navigationIconResource = R.drawable.ic_menu_white_24dp
-            setNavigationOnClickListener {
-                drawer.openDrawer(GravityCompat.START)
-            }
-        }
-        setSupportActionBar(toolbar)
 
         val navigationView = find<NavigationView>(R.id.navigation_drawer_main_activity)
         navigationView.setNavigationItemSelectedListener {
@@ -48,15 +36,13 @@ class MainActivity : AppCompatActivity(){
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
             android.R.id.home -> {
                 drawer.openDrawer(GravityCompat.START)
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
-    }
 
     override fun onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
--- app/src/main/kotlin/com/games/yshmgrt/gok/activities/mainactivity/views/fragments/LevelListFragment.kt
+++ app/src/main/kotlin/com/games/yshmgrt/gok/activities/mainactivity/views/fragments/LevelListFragment.kt
@@ -4,19 +4,24 @@ import android.graphics.Color
 import android.os.Bundle
 import android.support.constraint.ConstraintSet
 import android.support.v4.app.Fragment
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.games.yshmgrt.gok.R
+import com.games.yshmgrt.gok.activities.mainactivity.MainActivity
 import com.games.yshmgrt.gok.activities.mainactivity.views.adapters.MainViewRecyclerAdapter
 import com.games.yshmgrt.gok.loader.DataLoader
+import org.jetbrains.anko.*
+import org.jetbrains.anko.appcompat.v7.navigationIconResource
 import org.jetbrains.anko.appcompat.v7.toolbar
-import org.jetbrains.anko.backgroundColorResource
 import org.jetbrains.anko.constraint.layout.ConstraintSetBuilder
 import org.jetbrains.anko.constraint.layout.applyConstraintSet
 import org.jetbrains.anko.constraint.layout.constraintLayout
-import org.jetbrains.anko.matchParent
 import org.jetbrains.anko.recyclerview.v7.recyclerView
 import org.jetbrains.anko.support.v4.UI
 import org.jetbrains.anko.support.v4.dip
@@ -27,18 +32,31 @@ class LevelListFragment : Fragment() {
             constraintLayout {
                 lparams(width = matchParent, height = matchParent)
                 id = R.id.fragment_main_activity
+                val recycler = recyclerView {
+                    id = ViewGroup.generateViewId()
+                    adapter = MainViewRecyclerAdapter(DataLoader.levelInfoList)
+                    layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+                }.lparams(width = matchParent, height = 0)
+
                 val toolbar = toolbar {
                     id = R.id.toolbar_main_activity
                     backgroundColorResource = R.color.colorPrimary
                     setTitleTextColor(Color.WHITE)
                     title = "Hello, World!"
                     elevation = dip(8).toFloat()
+                    navigationIconResource = R.drawable.ic_menu_white_24dp
+                    setNavigationOnClickListener {
+                        (activity as MainActivity).drawer.openDrawer(GravityCompat.START)
+                        toast("Hello")
+                    }
                 }.lparams(width = matchParent)
-                val recycler = recyclerView {
-                    id = ViewGroup.generateViewId()
-                    adapter = MainViewRecyclerAdapter(DataLoader.levelInfoList)
-                    layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-                }.lparams(width = matchParent, height = 0)
+                (activity as MainActivity).apply {
+                    setSupportActionBar(toolbar)
+                    supportActionBar?.apply {
+                        setDisplayHomeAsUpEnabled(true)
+                        setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp)
+                    }
+                }
 
                 applyConstraintSet {
                     toolbar {
